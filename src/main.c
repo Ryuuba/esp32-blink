03392a3d9bada2edd6df19d7d4ec3ca2c0b8884c
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2023-07-20 16:47:28 -0600

    first commit

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/* Blink Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 22
+#define CONFIG_BLINK_PERIOD 1000
+
+
+void app_main(void)
+{
+    static uint8_t s_led_state = 0;
+    /* Configure the peripheral according to the LED type */
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(BLINK_GPIO, s_led_state);
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
